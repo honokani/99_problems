057f5ab833d2247d6168bc42499a0210cb74f9d1
show <honokani.kaoriga@gmail.com>
2018-04-19 06:04:47 +0000

    FIX error msg

--- p03/src/Lib.hs
+++ p03/src/Lib.hs
@@ -6,7 +6,7 @@ solveP03 :: IO ()
 solveP03 = print $ kthElememt [10, 20, 30, 40, 50] 2
 
 kthElememt :: [a] -> Int -> a
-kthElememt [] _ = error "empty list"
+kthElememt [] _ = error "list too short"
 kthElememt (x:_) 1 = x
 kthElememt (_:xs) n = case n<1 of
     True  -> error $ "there is no " ++ show n ++ "th position"
