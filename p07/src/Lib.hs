71992b40d425a77c108ef2af9962d057f6ca07e9
show <honokani.kaoriga@gmail.com>
2018-04-19 08:18:53 +0000

    FIX typo

--- p07/src/Lib.hs
+++ p07/src/Lib.hs
@@ -12,12 +12,12 @@ solveP07 = do
     print $ flatten $ B [ B [L 1, L 1], L 2, B [L 3, B [L 5, L 8]] ]
 
 flatten :: Tree a -> [a]
-flatten E         = []
+flatten ( E )     = []
 flatten ( B [] )  = []
 flatten ( B [E] ) = []
 flatten ( L x )     = [x]
 flatten ( B [L x] ) = [x]
 flatten ( B (x:xs) ) = case x of
-    E   ->              flatten (B xs)
-    b   -> flatten b ++ flatten (B xs)
+    E ->              flatten (B xs)
+    b -> flatten b ++ flatten (B xs)
 
