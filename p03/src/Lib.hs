f2b5d8340359658caeec0e63bbae5beb64e9fce3
show <honokani.kaoriga@gmail.com>
2018-04-19 06:02:46 +0000

    ADD p03

---
+++ p03/src/Lib.hs
@@ -0,0 +1,14 @@
+module Lib
+    ( solveP03
+    ) where
+
+solveP03 :: IO ()
+solveP03 = print $ kthElememt [10, 20, 30, 40, 50] 2
+
+kthElememt :: [a] -> Int -> a
+kthElememt [] _ = error "empty list"
+kthElememt (x:_) 1 = x
+kthElememt (_:xs) n = case n<1 of
+    True  -> error $ "there is no " ++ show n ++ "th position"
+    False -> kthElememt xs $ n-1
+
