499f8259071b50c4e57a3c3f335bf3fbe3b3c88c
show <honokani.kaoriga@gmail.com>
2018-04-23 08:48:45 +0000

    ADD p21

---
+++ p21/src/Lib.hs
@@ -0,0 +1,16 @@
+module Lib
+    ( solveP21 
+    ) where
+
+solveP21 :: IO ()
+solveP21 = do
+    print $ insertAt "alfa" ["a","b","c","d"] 2
+
+
+insertAt :: a -> [a] -> Int -> [a]
+insertAt x [] 1 = [x]
+insertAt _ [] _ = error "Out of bound!"
+insertAt x ally@(y:ys) n
+    | n==1       = x : ally
+    | otherwise  = y : insertAt x ys (n-1)
+
