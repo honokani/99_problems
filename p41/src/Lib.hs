6ad3e73297bf5965e311f9cc2d95b0dc2af01342
show <honokani.kaoriga@gmail.com>
2018-05-16 02:57:45 +0000

    UPDATE p41

--- p41/src/Lib.hs
+++ p41/src/Lib.hs
@@ -6,9 +6,12 @@ import Lib_p31 as P31
 
 solve41 :: IO ()
 solve41 = do
-    print $ goldbach 28
+    print $ goldbachList 9 33
 
 goldbach :: (Integral a) => a -> (a,a)
 goldbach n = head [ (x,y) | x <- candiP, y <- candiP, n==x+y]
     where candiP = takeWhile (<=n) P31.primes
+
+goldbachList l h = map goldbach candiL
+    where candiL = filter even [l..h]
 
