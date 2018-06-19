a077f997ffc7c39043a02812c2c52ec04f9cdf6e
show <honokani.kaoriga@gmail.com>
2018-06-19 00:05:42 +0000

    UPDATE p65

--- p65/src/Lib.hs
+++ p65/src/Lib.hs
@@ -25,31 +25,12 @@ layoutBinalyTree65 t = normalizePosition.fst $ decideLayout t 1 0
     where
         mDepth = maxDepth t
         decideLayout E _ _ = (E,0)
-        decideLayout (B x l r) currDepth here = (B (x,(currDepth,arm)) placedL placedR, here)
+        decideLayout (B x l r) currDepth here = (B (x,(currDepth,here)) placedL placedR, here)
             where
                 arm = if currDepth == mDepth then 0 else (2^(mDepth-currDepth-1)-1)
                 (placedL,wl) = decideLayout l (currDepth+1) (here-arm-1)
                 (placedR,wr) = decideLayout r (currDepth+1) (here+arm+1)
 
-normalizePosition :: (Integral a, Bounded a) => Tree (b,(a,a)) -> Tree (b,(a,a))
-normalizePosition t = positionShift t
-    where
-        getMinimumPos E = maxBound
-        getMinimumPos (B (_,(_,w)) l r) = foldr min maxBound [w,getMinimumPos l,getMinimumPos r]
-        n = getMinimumPos t
-        positionShift E = E
-        positionShift (B (x,(d,w)) l r) = (B (x,(d,(w-n))) (positionShift l) (positionShift r))
-
-
-
-widthGrandBranchs E = (0,0)
-widthGrandBranchs (B x E E) = (0,0)
-widthGrandBranchs (B x l r) = (nl,nr)
-    where
-        (nll,nlr) = widthGrandBranchs l
-        (nrl,nrr) = widthGrandBranchs r
-        nl = (+1).(*2) $ max nll nlr
-        nr = (+1).(*2) $ max nrl nrr
 
 maxDepth :: (Integral a) => Tree b -> a
 maxDepth E = 0
@@ -57,4 +38,13 @@ maxDepth (B _ l r) = max lh rh + 1
     where
         lh = maxDepth l
         rh = maxDepth r
+
+normalizePosition :: (Integral a, Bounded a) => Tree (b,(a,a)) -> Tree (b,(a,a))
+normalizePosition t = positionShift t
+    where
+        getMinimumPos E = maxBound
+        getMinimumPos (B (_,(_,w)) l r) = foldr min maxBound [w,getMinimumPos l,getMinimumPos r]
+        n = getMinimumPos t - 1
+        positionShift E = E
+        positionShift (B (x,(d,w)) l r) = (B (x,(d,(w-n))) (positionShift l) (positionShift r))
 
